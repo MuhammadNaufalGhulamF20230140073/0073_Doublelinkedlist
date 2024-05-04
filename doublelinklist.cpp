faad8d129ac54ecf6d6b18532c37ebc7fa536892
MuhammadNaufalGhulamF20230140073 <naufalfakhrudddin@gmail.com>
2024-05-04 23:52:36 +0700

    mengatur  next dari newNode untuk menunjuk ke current

--- doublelinklist.cpp
+++ doublelinklist.cpp
@@ -32,3 +32,15 @@ void addNode() {
     newNode->prev = NULL; 
     START = newNode; 
     }
+
+    else
+    {
+        Node* current = START; 
+        Node* previous = NULL; 
+        while (current != NULL && current->noMhs < newNode->noMhs)
+        { 
+            previous = current; 
+            current = current->next; 
+        }
+
+newNode->next = current; 
