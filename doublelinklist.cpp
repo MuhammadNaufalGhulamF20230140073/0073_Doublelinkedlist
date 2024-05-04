d66fc2e47e8955e733ca9273d6744c6a8e84c6ba
MuhammadNaufalGhulamF20230140073 <naufalfakhrudddin@gmail.com>
2024-05-04 23:58:38 +0700

    Menambahkan Prosedur search dengan tipe data Boolean

--- doublelinklist.cpp
+++ doublelinklist.cpp
@@ -61,3 +61,14 @@ newNode->prev = previous;
         }
     }
 }
+
+bool search(int rollNo, Node** previous, Node** current) {
+    *previous = NULL;
+    *current = START;
+    while (*current != NULL && (*current)->noMhs != rollNo)
+    {
+        *previous = *current;
+        *current = (*current)->next;
+    }
+    return (*current != NULL);
+}
