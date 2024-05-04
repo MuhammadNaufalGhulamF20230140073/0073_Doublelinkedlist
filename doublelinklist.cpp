4a9136f456716e84691ac0db5e20d22a9d2edbfd
MuhammadNaufalGhulamF20230140073 <naufalfakhrudddin@gmail.com>
2024-05-04 23:40:27 +0700

    memeriksa apakah linked list sebelumnya tidak kosong, mengubah  prev dari START  sebelumnya) untuk  ke newNode.

--- doublelinklist.cpp
+++ doublelinklist.cpp
@@ -24,5 +24,9 @@ void addNode() {
             cout << "\033[31mDuplicate roll numbers allowed\033[0m" << endl;
             return;
     }
+  newNode->next = START; 
+        if (START != NULL)
+        {
+            START->prev = newNode; 
+        }
 
-    
